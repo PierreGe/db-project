77c1672121107029e8e856700890b9759787a0fb
Pierre Gerard <piegerar@ulb.ac.be>
2015-04-24 17:14:00 +0200

    [add] insertion station, beware sql injection

--- src/populate_db.py
+++ src/populate_db.py
@@ -6,6 +6,9 @@ from sqlite3 import Connection
 db = Connection("test.sqlite")
 
 insert_bike = lambda id, entry_date, model, usable: "INSERT INTO bike (id,entry_date,model,usable) VALUES (%d,'%s','%s','%s');" % (id, entry_date, model, usable)
+insert_station = lambda id, payment, capacity, latitude, longitude, name: "INSERT INTO station (id, payment, capacity, latitude, longitude, name) VALUES (%d,'%s','%d','%f', '%f', '%s');" % (id, payment, capacity, latitude, longitude, name)
+
+
 
 for bike in DictReader(open("data/villos.csv"), delimiter=';'):
     query = insert_bike(
@@ -15,3 +18,15 @@ for bike in DictReader(open("data/villos.csv"), delimiter=';'):
         model=bike['mode\xcc\x80le'])
     res = db.execute(query)
     print query, [row for row in res]
+
+for station in DictReader(open("data/stations.csv"), delimiter=';'):
+    query = insert_station(
+        id=int(station['nume\xcc\x81ro']),
+        payment=(station['borne de paiement'] == 'True'),
+        capacity=int(station['capacite\xcc\x81']),
+        latitude=float(station['coordonne\xcc\x81e X']),
+        longitude=float(station['coordonne\xcc\x81e Y']),
+        name=(station['nom']))
+    res = db.execute(query)
+    print query, [row for row in res]
+
