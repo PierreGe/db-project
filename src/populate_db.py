79f122de31209fadd2fdb0f7f67186a21122660d
iTitou <moiandme@gmail.com>
2015-04-27 14:51:39 +0200

    [fix] Param order

--- src/populate_db.py
+++ src/populate_db.py
@@ -27,8 +27,8 @@ def insert_bikes(db, input_file="data/villos.csv"):
         (
             int(bike['nume\xcc\x81ro']),
             bike['mise en service'],
+            bike['mode\xcc\x80le'],
             bike['fonctionne'] == 'True',
-            bike['mode\xcc\x80le']
         ) for bike in DictReader(open(input_file), delimiter=';')
     )
     with db:
