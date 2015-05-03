2aabbcdb7050228dae806f7be4d45656c99efc75
iTitou <moiandme@gmail.com>
2015-05-04 01:44:51 +0200

    [ref] SQL code layout

--- src/createDB.sql
+++ src/createDB.sql
@@ -3,9 +3,12 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE IF NOT EXISTS station (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     payment BOOLEAN NOT NULL,
-    capacity INTEGER NOT NULL CHECK(capacity >= 0),
-    latitude REAL NOT NULL CHECK(-90<=latitude AND latitude<=90),
-    longitude REAL NOT NULL CHECK(-180<=longitude AND longitude<=180),
+    capacity INTEGER NOT NULL
+        CHECK(capacity >= 0),
+    latitude REAL NOT NULL
+        CHECK(-90<=latitude AND latitude<=90),
+    longitude REAL NOT NULL
+        CHECK(-180<=longitude AND longitude<=180),
     name VARCHAR(32) NOT NULL,
 
     UNIQUE(latitude, longitude)
@@ -13,7 +16,8 @@ CREATE TABLE IF NOT EXISTS station (
 
 CREATE TABLE IF NOT EXISTS bike (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    entry_date VARCHAR(20) NOT NULL CHECK (entry_date IS strftime(entry_date)),
+    entry_date VARCHAR(20) NOT NULL
+        CHECK(entry_date IS strftime(entry_date)),
     model VARCHAR(32) NOT NULL,
     usable BOOLEAN NOT NULL
 );
@@ -22,7 +26,8 @@ CREATE TABLE IF NOT EXISTS user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     password VARCHAR(64) NOT NULL,
     card VARCHAR(64) NOT NULL,
-    expire_date VARCHAR(20) CHECK (expire_date IS strftime(expire_date))
+    expire_date VARCHAR(20) 
+        CHECK(expire_date IS strftime(expire_date))
 );
 
 CREATE TABLE IF NOT EXISTS subscriber (
@@ -38,11 +43,12 @@ CREATE TABLE IF NOT EXISTS subscriber (
 
 CREATE TABLE IF NOT EXISTS trip (
     departure_station_id INTEGER NOT NULL,
-    departure_date VARCHAR(20) NOT NULL CHECK (departure_date IS strftime(departure_date)),
-
+    departure_date VARCHAR(20) NOT NULL
+        CHECK (departure_date IS strftime(departure_date)),
     arrival_station_id INTEGER,
-    arrival_date VARCHAR(20) CHECK (arrival_date IS strftime(arrival_date)) CHECK(arrival_date >= departure_date),
-  
+    arrival_date VARCHAR(20)
+        CHECK(arrival_date IS strftime(arrival_date))
+        CHECK(arrival_date >= departure_date),
     user_id INTEGER NOT NULL,
     bike_id INTEGER NOT NULL,
 
