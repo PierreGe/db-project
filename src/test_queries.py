fd3309828f93e6e7272ef245055416636266ee40
iTitou <moiandme@gmail.com>
2015-05-03 14:43:52 +0200

    [enh] Easier fixtures for 6 queries

--- src/test_queries.py
+++ src/test_queries.py
@@ -6,7 +6,8 @@ from populate_db import (
     insert_trips,
     Connection
 )
-from models import Database
+from models import Database, fetch_all
+from datetime import datetime, timedelta
 
 def load_query(i):
     """Load SQL without comments from queries/r<i>.sql"""
@@ -14,16 +15,89 @@ def load_query(i):
         lambda line: not line.strip().startswith("--"),
         open("queries/r%d.sql" % i)))
 
-conn = None
+def exec_query(q):
+    print q
+    res = fetch_all(lambda *args: args, conn.execute(q))
+    print res
+    return res
 
+def insert_fixtures(conn):
+    db = Database(conn)
+
+    # Time base
+    t = lambda **args: datetime(2012, 12, 21, 11, 11, 11) + timedelta(**args)
+
+    Admin = db.User.create(id=0, expire_date=None, password="admin")
+
+    # A, B, C: Registred user, B and C in Ixelles
+    A = db.User.create(id=1, expire_date=t(days=365),
+        password="a", rfid="123456789",
+        firstname="A", lastname="A",
+        address="123 rue du Soleil, 1190 Forest")
+    B = db.User.create(id=2, expire_date=t(days=237),
+        password="b", rfid="456789123",
+        firstname="B", lastname="B",
+        address="123 chausee de Boondael, 1050 Ixelles")
+    C = db.User.create(id=3, expire_date=t(days=344),
+        password="c", rfid="789123456",
+        firstname="C", lastname="C",
+        address="123 avenue de l'universite, 1050 Ixelles")
+
+    # D, E: Temporary users
+    D = db.User.create(password="d", expire_date=t(days=1))
+    E = db.User.create(password="e", expire_date=t(days=5))
+
+    # 5 bikes
+    Bikes = [db.Bike.create() for i in range(5)]
+
+    # 3 stations, including Flagey
+    Flagey = db.Station.create(name="FLAGEY",
+        latitude=50.8279234774, longitude=4.37191500367,
+        payment=True, capacity=25)
+    ULB = db.Station.create(name="ULB",
+        latitude=50.8125663191, longitude=4.37914601751,
+        payment=True, capacity=27)
+    Arsenal = db.Station.create(name="ARSENAL",
+        latitude=50.8265178652, longitude=4.39705753109,
+        payment=True, capacity=25)
+
+    # All bikes initially in Flagey by admin
+    for bike in Bikes:
+        db.Trip.create(
+            user_id=Admin.id, bike_id=bike.id,
+            departure_station_id=Flagey.id, departure_date=t(seconds=0),
+            arrival_station_id=Flagey.id, arrival_date=t(seconds=0))
+
+    # B: Flagey -> ULB, villo1
+    db.Trip.create(
+        user_id=B.id, bike_id=Bikes[0].id,
+        departure_station_id=Flagey.id, departure_date=t(seconds=1),
+        arrival_station_id=ULB.id, arrival_date=t(seconds=1200))
+
+    # B: ULB -> Arsenal, villo1
+    db.Trip.create(
+        user_id=B.id, bike_id=Bikes[0].id,
+        departure_station_id=Flagey.id, departure_date=t(days=2),
+        arrival_station_id=ULB.id, arrival_date=t(days=2, seconds=1200))
+
+    # A: Flagey -> ULB, villo2
+    db.Trip.create(
+        user_id=A.id, bike_id=Bikes[1].id,
+        departure_station_id=Flagey.id, departure_date=t(seconds=54000),
+        arrival_station_id=ULB.id, arrival_date=t(seconds=58000))
+
+    # A: Flagey -> ULB, villo3
+    db.Trip.create(
+        user_id=A.id, bike_id=Bikes[2].id,
+        departure_station_id=Flagey.id, departure_date=t(days=12, seconds=54000),
+        arrival_station_id=ULB.id, arrival_date=t(days=12, seconds=58000))
+
+conn = None
 def setup_function(*args):
     global conn
     conn = Connection(':memory:')
     create_tables(conn)
-    insert_bikes(conn, "queries/villos.csv")
-    insert_stations(conn, "queries/stations.csv")
-    insert_users(conn, "queries/users.xml")
-    insert_trips(conn, "queries/trips.csv")
+    insert_fixtures(conn)
 
 def teardown_function(*args):
     global conn
@@ -32,14 +106,15 @@ def teardown_function(*args):
 def test_expected_data():
     """Test fixtures content"""
     db = Database(conn)
-    assert db.User.count() == 8
-    assert db.Bike.count() == 11
-    assert db.Station.count() == 4
-    assert db.Trip.count() == 12
+    assert db.User.count() == 6
+    assert db.Bike.count() == 5
+    assert db.Station.count() == 3
+    assert db.Trip.count() == 4 + 5
 
 def test_r1():
-    Q = load_query(1)
-    res = [row for row in conn.execute(Q)]
-    print Q
-    print res
-    assert res == [(1, "Mercedes", "BOURDON")]
+    res = exec_query(load_query(1))
+    assert res == [(2, "B", "B")]
+
+def test_r2():
+    res = exec_query(load_query(2))
+    assert res == [(1,), (2,)]
