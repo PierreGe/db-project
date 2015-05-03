29e251bdc823277301d2ae6c08511120a31f58d6
iTitou <moiandme@gmail.com>
2015-05-03 14:48:15 +0200

    [fix] Do nto include admin in R2

--- src/queries/r2.sql
+++ src/queries/r2.sql
@@ -1,6 +1,7 @@
 -- Les utilisateurs ayant utilise Villo au moins 2 fois
 
 SELECT id from user,trip 
-    WHERE user.id = trip.user_id 
+    WHERE user.id = trip.user_id  AND
+          user.expire_date IS NOT NULL
     GROUP BY user.id 
     HAVING count(trip.user_id)>1;
