744b940bc5b48295dcbba057a084267efb458d3c
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-04 18:44:19 +0200

    [add] query 6 (not tested yet)

--- src/queries/r6.sql
+++ src/queries/r6.sql
@@ -2,3 +2,11 @@
 -- (un meme velo peut-etre comptabilise plusieurs fois) et le nombre
 -- d'utilisateurs differents ayant utilise la station et ce pour toutes les
 -- stations ayant ete utilisees au moins 10 fois.
+
+
+
+SELECT station.name, COUNT(trip.arrival_station_id ),COUNT(DISTINCT trip.user_id) FROM station,trip WHERE trip.arrival_station_id = station.id GROUP BY station.name HAVING COUNT(trip.arrival_station_id >10);
+
+
+
+
