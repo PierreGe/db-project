9beb457d8a95e7b0560e072696d38c407c878304
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-04 22:55:24 +0200

    [add] unfinished r5

--- src/queries/r5.sql
+++ src/queries/r5.sql
@@ -1,3 +1,8 @@
 -- Les utilisateurs, la date d'inscription, le nombre total de trajet effectues,
 -- la distance totale parcourue et la distance moyenne parcourue par trajet,
 -- classes en fonction de la distance totale parcourue
+
+
+-- WARNING : date d'inscription et distance
+
+SELECT subscriber.firstname,subscriber.lastname, COUNT(trip.arrival_station_id) FROM subscriber,trip WHERE trip.arrival_station_id = subscriber.user_id GROUP BY subscriber.firstname,subscriber.lastname ORDER BY COUNT(trip.arrival_station_id);
