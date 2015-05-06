a0c047aa4de180a3897e3e960d2fac03a5d3e4b8
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-06 11:17:15 +0200

    [enh] queries

--- src/queries/r5.sql
+++ src/queries/r5.sql
@@ -3,6 +3,6 @@
 -- classes en fonction de la distance totale parcourue
 
 
--- WARNING : date d'inscription et distance
+-- WARNING :  distance manquante
 
--- SELECT subscriber.firstname,subscriber.lastname, COUNT(trip.arrival_station_id) FROM subscriber,trip WHERE trip.arrival_station_id = subscriber.user_id GROUP BY subscriber.firstname,subscriber.lastname ORDER BY COUNT(trip.arrival_station_id);
+SELECT subscriber.firstname,subscriber.lastname, subscriber.entry_date, COUNT(trip.arrival_station_id) FROM subscriber,trip WHERE trip.arrival_station_id = subscriber.user_id GROUP BY subscriber.firstname,subscriber.lastname ORDER BY COUNT(trip.arrival_station_id);
