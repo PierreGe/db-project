eb99858877ebc6d30f23e833eea28d7eb3aabdf4
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-08 16:50:42 +0200

    [add] some more algebra

--- src/queries/r1.sql
+++ src/queries/r1.sql
@@ -1,7 +1,12 @@
 -- Les utilisateurs habitant Ixelles ayant utilise un Villo de la station Flagey
 
-SELECT subscriber.user_id,firstname,lastname FROM subscriber,trip
-    INNER JOIN station ON trip.departure_station_id = station.id
-    WHERE subscriber.user_id = trip.user_id AND 
-          station.name="FLAGEY" AND subscriber.address_zipcode=1050
-    GROUP BY subscriber.user_id;
+SELECT DISTINCT subscriber.user_id,
+                subscriber.firstname,
+                subscriber.lastname
+FROM   subscriber,
+       trip
+       INNER JOIN station
+               ON trip.departure_station_id = station.id
+WHERE  subscriber.user_id = trip.user_id
+       AND station.NAME = "FLAGEY"
+       AND subscriber.address_zipcode = 1050;
