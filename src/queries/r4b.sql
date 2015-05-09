24e673adf6b1357eb74645ea8d40e03ee0958f54
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-09 22:29:39 +0200

    [add] r4 v2

--- src/queries/r4b.sql
+++ src/queries/r4b.sql
@@ -1,12 +1,12 @@
---- Requete lente possible d'ecrire en algebre relationnel
-SELECT DISTINCT t1.bike_id
-FROM   trip t1, 
-       trip t2 
-WHERE  t1.bike_id = t2.bike_id 
+SELECT DISTINCT t1.bike_id 
+FROM   trip t1 
+       INNER JOIN trip t2 
+               ON t1.bike_id = t2.bike_id 
+       LEFT JOIN trip AS t3 
+              ON t1.bike_id = t3.bike_id 
+                 AND t1.arrival_date < t3.departure_date 
+                 AND t3.departure_date < t2.departure_date 
+WHERE  t1.arrival_station_id != t1.departure_station_id 
        AND t1.arrival_station_id != t2.departure_station_id 
        AND t1.arrival_date < t2.departure_date 
-       AND NOT EXISTS (SELECT t3.departure_station_id 
-                       FROM   trip t3 
-                       WHERE  t1.bike_id = t3.bike_id 
-                              AND t1.arrival_date < t3.departure_date 
-                              AND t3.departure_date < t2.departure_date); 
+       AND t3.bike_id IS NULL; 
