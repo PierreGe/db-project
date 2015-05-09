42398ce2502bf1006fff6a00d18adb161cf75617
iTitou <moiandme@gmail.com>
2015-05-09 14:08:13 +0200

    [fix] Ignore admin users in R2

--- src/queries/r2.sql
+++ src/queries/r2.sql
@@ -1,5 +1,6 @@
 -- Les utilisateurs ayant utilise Villo au moins 2 fois
 
-SELECT user_id,tripcount FROM (
-    SELECT user_id,COUNT(bike_id) AS tripcount FROM trip GROUP BY bike_id)
-WHERE tripcount>=2;
+SELECT user_id FROM (
+    SELECT user_id,COUNT(bike_id) AS tripcount FROM trip GROUP BY user_id)
+INNER JOIN user ON user_id=user.id
+WHERE tripcount>=2 AND user.expire_date NOT NULL;
