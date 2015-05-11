03b132578a4b66a8c7fbcb93fc4266c0495780d1
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-11 18:52:44 +0200

    [add] r4 gotcha git add queries/r4b.sql

--- src/queries/r4b.sql
+++ src/queries/r4b.sql
@@ -1,12 +1,13 @@
-SELECT DISTINCT t1.bike_id 
-FROM   trip t1 
-       INNER JOIN trip t2 
-               ON t1.bike_id = t2.bike_id 
-       LEFT JOIN trip AS t3 
-              ON t1.bike_id = t3.bike_id 
-                 AND t1.arrival_date < t3.departure_date 
-                 AND t3.departure_date < t2.departure_date 
-WHERE  t1.arrival_station_id != t1.departure_station_id 
-       AND t1.arrival_station_id != t2.departure_station_id 
-       AND t1.arrival_date < t2.departure_date 
-       AND t3.bike_id IS NULL; 
+SELECT DISTINCT t1.bike_id
+FROM   trip t1
+       INNER JOIN trip t2
+               ON t1.bike_id = t2.bike_id
+       LEFT JOIN trip AS t3
+              ON t1.bike_id = t3.bike_id
+                AND t1.arrival_date < t3.arrival_date
+                 AND t3.arrival_date < t2.departure_date
+WHERE   t1.arrival_station_id != t2.departure_station_id
+       AND t1.arrival_date <= t2.departure_date
+       AND t3.bike_id IS NULL;
+
+
