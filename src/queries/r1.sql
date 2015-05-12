d46d1f130369f53829f6fe77e6944926110eb2ba
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-12 22:46:20 +0200

    [ref] q1

--- src/queries/r1.sql
+++ src/queries/r1.sql
@@ -3,10 +3,10 @@
 SELECT DISTINCT subscriber.user_id,
                 subscriber.firstname,
                 subscriber.lastname
-FROM   subscriber,
-       trip
+FROM   subscriber
+       INNER JOIN trip
+               ON subscriber.user_id = trip.user_id
        INNER JOIN station
-               ON trip.departure_station_id = station.id
-WHERE  subscriber.user_id = trip.user_id
-       AND station.NAME = "FLAGEY"
+               ON trip.departure_station_id = station.id 
+WHERE  station.NAME = "FLAGEY"
        AND subscriber.address_zipcode = 1050;
