285cc013cb6296d344a0967aa4a983cd8c886c8d
Pierre Gerard <piegerar@ulb.ac.be>
2015-05-14 23:07:57 +0200

    [ref] yet another R4 refactoring

--- src/queries/r4b.sql
+++ src/queries/r4b.sql
@@ -6,12 +6,12 @@ SELECT DISTINCT t1.bike_id
 FROM   trip t1
        INNER JOIN trip t2
                ON t1.bike_id = t2.bike_id
+                  AND t1.arrival_date <= t2.departure_date
+                  AND t1.arrival_station_id != t2.departure_station_id
        LEFT OUTER JOIN trip AS t3
-              ON t1.bike_id = t3.bike_id
-                 AND t1.arrival_date < t3.arrival_date
-                 AND t3.arrival_date < t2.departure_date
-WHERE  t1.arrival_station_id != t2.departure_station_id
-       AND t1.arrival_date <= t2.departure_date
-       AND t3.bike_id IS NULL;
+                    ON t1.bike_id = t3.bike_id
+                       AND t1.arrival_date < t3.arrival_date
+                       AND t3.arrival_date < t2.departure_date
+WHERE  t3.bike_id IS NULL;
 
 
